2ef9cb4e49568aa7fb6db1bff0c95a0027b2bcea
ElectCarlKawk <107461655+ElectCarlKawk@users.noreply.github.com>
2022-06-14 15:37:08 +0700

    Update and rename README.md to code for poor.js

---
+++ code for poor.js
@@ -0,0 +1,14 @@
+idk this code
+              }
+             }
+            }
+star program 196475765 robux for user <kirillnkling>
+            }
+           }
+          } 
+delete user Roblox ceo give kirillnkling Roblox ceo
+       
+
+hack mode 2 user kirillnkling Obbyillinkling
+
+(end)    
